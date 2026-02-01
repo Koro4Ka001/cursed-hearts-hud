e2ca1d2022d571b472d422897a93500bff41a640
Koro4Ka001 <137198794+Koro4Ka001@users.noreply.github.com>
2026-02-01 21:02:31 +0300

    Update config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 // === КОНФИГУРАЦИЯ API ===
 // ЗАМЕНИТЬ НА РЕАЛЬНЫЙ URL ПОСЛЕ ПУБЛИКАЦИИ WEB APP
 
-export const GOOGLE_API_URL = 'ВАШ_URL_СЮДА';
+export const GOOGLE_API_URL = 'https://script.google.com/macros/s/AKfycbzu42HPs3o4hoIxAPP4mB87fBK21_ENOlVcJPJxqP2wj18N250_XSA75dd_yDzonEJU/exec';
 
 // Таймаут запросов (мс)
 export const API_TIMEOUT = 10000;
